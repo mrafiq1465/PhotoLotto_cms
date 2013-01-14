6a3e3ddbf9b8ec5ae273134204ad7e6bb9910785
raf-ampush <raf.islam@ampushmedia.com>
2013-01-15 00:30:06 +0600

    fixes

---
+++ app/Config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Abdullah
+ * Date: 1/12/13
+ * Time: 12:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$config['s3_img_url'] = 'http://s3.appevent.com/';
+
+define('S3_IMG_URL', 'http://s3.appevent.com/');
