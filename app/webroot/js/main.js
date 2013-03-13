84c14f8eb042880081330a8ea9afa78d5c3fbe47
raf-ampush <raf.islam@ampush.com>
2013-03-12 22:50:24 -0700

    date range change

--- app/webroot/js/main.js
+++ app/webroot/js/main.js
@@ -40,8 +40,8 @@ $(document).ready(function() {
     $(":date").dateinput({
         format: 'yyyy-mm-dd',	// the format displayed for the user
         selectors: true,             	// whether month/year dropdowns are shown
-        min: 0,                    // min selectable day (100 days backwards)
-        max: 100,                    	// max selectable day (100 days onwards)
+        min: -100,                    // min selectable day (100 days backwards)
+        max: 2000,                    	// max selectable day (100 days onwards)
         offset: [-10, 0],            	// tweak the position of the calendar
         speed: 'fast',               	// calendar reveal speed
         firstDay: 1                  	// which day starts a week. 0 = sunday, 1 = monday etc..
