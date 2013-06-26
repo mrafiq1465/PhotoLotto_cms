a4fc328461c3d4fe371b6d7027583a2381749073
root <root@localhost.localdomain>
2013-06-26 16:11:15 +0600

    space added

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -583,7 +583,7 @@ class EventsController extends AppController
         $image_columnA = $email_config['image_columnA'];
 
         //image right configurable with html/custom href N.B: Do not remove img tag from here!
-        $image_columnB = '<a href="#"> <img style="display: block; src="'.$email_config['image_columnB'].'alt="Pixta"/></a>';
+        $image_columnB = '<a href="#"> <img style="display: block; src="'.$email_config['image_columnB'].'  alt="Pixta"/></a>';
         $email_from = $email_config['email_from'];
 
 
@@ -632,7 +632,7 @@ class EventsController extends AppController
                 } else if(isset($event_config['EventEmailConfig']['image_right']) && trim($event_config['EventEmailConfig']['image_right'])!=='') {
 
                     $href = isset($event_config['EventEmailConfig']['image_right'])? isset($event_config['EventEmailConfig']['image_right']) : '#';
-                    $image_columnB = '<a href="'.$href.'"> <img style="display: block; src="'.$host . $event_config['EventEmailConfig']['image_right'].'alt="Pixta"/></a>';
+                    $image_columnB = '<a href="'.$href.'"> <img style="display: block; src="'.$host . $event_config['EventEmailConfig']['image_right'].' alt="Pixta"/></a>';
 
                 }
                 //newly added end
@@ -641,7 +641,7 @@ class EventsController extends AppController
                     $email_from =  $event_config['EventEmailConfig']['email_from'];
                 }
             }
-            
+
             if (empty($_GET['email_to'])) {
                 die(json_encode(array('error' => 'email not given')));
             }
