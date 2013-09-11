56144adcd50664ad87761aa30db5d5b647b24ff0
Rick FlyDigital <henrique@flydigital.com.au>
2013-09-12 09:42:59 +1000

    changed email from no-replay to into

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -918,9 +918,9 @@ class EventsController extends AppController
 
 
                 App::uses('CakeEmail', 'Network/Email');
-                $email = new CakeEmail();
+                $email = new CakeEmail('smtp');
 //               $email->config('default');
-                  $email->from($email_from);
+               // $email->from($email_from);
                 
                 $email->to($to);
                 $email->subject($_GET['subject']);
